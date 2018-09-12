3ee056ef921b450c09ea0e59409205c8426d61ef
svichas <stefanos@vichas.org>
2018-09-12 22:44:29 +0300

    Updates

--- storage/jsbin/main.js
+++ storage/jsbin/main.js
@@ -1,10 +1,47 @@
 
 $(function() {
-    
     $(".section").eq(0).addClass("active");
-
-    $('.mainContainer').on('mousewheel', function(event) {
-        console.log(event.deltaX, event.deltaY, event.deltaFactor);
-    });
-
+    setpage();
 });
+
+window.addEventListener('wheel', function(e) {
+  if (e.deltaY < 0) {
+      prevpage();
+  }
+  if (e.deltaY > 0) {
+      nextpage();
+  }
+});
+
+function setpage() {
+    var foundActive = false;
+    $(".section").removeClass("nextFromActive");
+    $(".section").each(function(e) {
+        if (foundActive) {
+            return $(this).addClass("nextFromActive");
+        }
+        if ($(this).hasClass("active")) foundActive = true;
+    });
+    $(".currentPage").html($(".section.active").index()+1);
+    $(".pageNumber").html($(".section").length);
+}
+
+function nextpage() {
+    // up scroll
+    var nextsect = $(".section.active").next(".section");
+    if (nextsect.length) {
+        $(".section.active").removeClass("active");
+        nextsect.addClass("active")
+        setpage();
+    }
+}
+
+function prevpage() {
+    // down scroll
+    var prevsect = $(".section.active").prev(".section");
+    if (prevsect.length) {
+        $(".section.active").removeClass("active");
+        prevsect.addClass("active");
+        setpage();
+    }
+}
