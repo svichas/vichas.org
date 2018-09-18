4206119b35e9ad9d41e123acf333861b17ada981
Stefanos Vichas <stefanos.vichas@atcom.gr>
2018-09-18 10:37:52 +0300

    Added scroll prevention + comments on js

--- storage/jsbin/main.js
+++ storage/jsbin/main.js
@@ -1,7 +1,16 @@
 
+// variable for preventing user changing pages too fast.
+var allowPageChange = false;
+
 $(function() {
+    
+    // set first section active
     $(".section").eq(0).addClass("active");
+
+    // add loaded class when dom is loaded for animations
     $("body").addClass("loaded");
+
+    // change pages with keys function
     $("body").keydown(function(e) {
         if (e.key == "ArrowUp") {
             prevpage();
@@ -9,21 +18,35 @@ $(function() {
             nextpage();
         }
     });
+
+    // render pagination & other ...
     setpage();
+
 });
+
 
 
 
 window.addEventListener('wheel', function(e) {
-  if (e.deltaY < 0) {
-      prevpage();
-  }
-  if (e.deltaY > 0) {
-      nextpage();
-  }
+
+    // prevent user from change pages too fast
+    if (!allowPageChange) return;
+
+    // check scroll direction
+	if (e.deltaY < 0) {
+        // case scroll up
+		prevpage();
+	}
+	if (e.deltaY > 0) {
+        // case scroll down
+		nextpage();
+	}
+
 });
 
 function setpage() {
+
+    // setting a class to all next sections from active section.
     var foundActive = false;
     $(".section").removeClass("nextFromActive");
     $(".section").each(function(e) {
@@ -32,33 +55,57 @@ function setpage() {
         }
         if ($(this).hasClass("active")) foundActive = true;
     });
+
+
+    // rendering page 
     $(".currentPage").html($(".section.active").index()+1);
     $(".pageNumber").html($(".section").length);
 
+    // add & remove class scrolled to body for animations
     if ($(".section.active").index()) {
         $("body").addClass("scrolled");
     } else {
         $("body").removeClass("scrolled");
     }
 
+    // allow user to scroll page after 1second
+    setInterval(function() {
+        allowPageChange = true;
+    }, 1000);
+
 }
 
+
+// method for next page
 function nextpage() {
-    // up scroll
+    // next section that we want to scroll on selection
     var nextsect = $(".section.active").next(".section");
+
     if (nextsect.length) {
+        // if next section is found deactivete current section active next section
         $(".section.active").removeClass("active");
-        nextsect.addClass("active")
+        nextsect.addClass("active");
+
+        // deactivate scrolling
+        allowPageChange = false;
         setpage();
     }
 }
 
+
+// method for prev page
 function prevpage() {
-    // down scroll
+    // prev section that we want to scroll on selection
     var prevsect = $(".section.active").prev(".section");
+
     if (prevsect.length) {
+        // if prev section is found deactivete current section active prev section
         $(".section.active").removeClass("active");
         prevsect.addClass("active");
+
+        // deactivate scrolling
+        allowPageChange = false;
         setpage();
+        
     }
 }
